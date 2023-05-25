b05b8d2fbe8415caa1152d5642069e5a25f1c1d3
mahdi niknejad <m.niknejad@aut.ac.ir>
2023-05-25 13:57:59 -0700

    Try to add serach bar with mendable

---
+++ _includes/search.tsx
@@ -0,0 +1,10 @@
+
+import { MendableFloatingButton } from '@mendable/search'
+
+<MendableFloatingButton
+    style={{ darkMode: true, accentColor: #8559F4 }}
+    floatingButtonStyle={{ backgroundColor: #8559F4, color: #ffffff }}
+    placeholder="چطوری تو کلاس ثبت نام کنم؟"
+    showSimpleSearch={true}
+    dialogPlaceholder="دنبال چی میگردی؟"
+    anon_key={"70c47b61-f58d-4b6e-82d2-6ee6ad58cc7c"} />
